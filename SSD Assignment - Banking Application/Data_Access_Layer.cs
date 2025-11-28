b6fe3f99535c542e3a86d0d29bec2a68efb48e75
EvanBradyS00238375 <S00238375@atu.ie>
2025-11-28 12:08:55 +0000

    Finished data encyption and used prepared statements on sql queries

--- SSD Assignment - Banking Application/Data_Access_Layer.cs
+++ SSD Assignment - Banking Application/Data_Access_Layer.cs
@@ -10,14 +10,15 @@ namespace Banking_Application
 {
     public class Data_Access_Layer
     {
-
         private List<Bank_Account> accounts;
         public static String databaseName = "Banking Database.db";
         private static Data_Access_Layer instance = new Data_Access_Layer();
+        private EncryptionHelper encryptionHelper;
 
-        private Data_Access_Layer()//Singleton Design Pattern (For Concurrency Control) - Use getInstance() Method Instead.
+        private Data_Access_Layer()
         {
             accounts = new List<Bank_Account>();
+            encryptionHelper = EncryptionHelper.GetInstance();
         }
 
         public static Data_Access_Layer getInstance()
@@ -27,7 +28,6 @@ namespace Banking_Application
 
         private SqliteConnection getDatabaseConnection()
         {
-
             String databaseConnectionString = new SqliteConnectionStringBuilder()
             {
                 DataSource = Data_Access_Layer.databaseName,
@@ -35,7 +35,6 @@ namespace Banking_Application
             }.ToString();
 
             return new SqliteConnection(databaseConnectionString);
-
         }
 
         private void initialiseDatabase()
@@ -44,9 +43,8 @@ namespace Banking_Application
             {
                 connection.Open();
                 var command = connection.CreateCommand();
-                command.CommandText =
-                @"
-                    CREATE TABLE IF NOT EXISTS Bank_Accounts(    
+                command.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Bank_Accounts(
                         accountNo TEXT PRIMARY KEY,
                         name TEXT NOT NULL,
                         address_line_1 TEXT,
@@ -59,9 +57,7 @@ namespace Banking_Application
                         interestRate REAL
                     ) WITHOUT ROWID
                 ";
-
                 command.ExecuteNonQuery();
-                
             }
         }
 
@@ -71,28 +67,30 @@ namespace Banking_Application
                 initialiseDatabase();
             else
             {
-
                 using (var connection = getDatabaseConnection())
                 {
                     connection.Open();
                     var command = connection.CreateCommand();
-                    command.CommandText = "SELECT * FROM Bank_Accounts";
+                    command.CommandText = "SELECT * FROM Bank_Accounts WHERE 1 = @alwaysTrue";
+                    command.Parameters.AddWithValue("@alwaysTrue", 1);
                     SqliteDataReader dr = command.ExecuteReader();
-                    
-                    while(dr.Read())
-                    {
 
+
+                    while (dr.Read())
+                    {
                         int accountType = dr.GetInt16(7);
 
-                        if(accountType == Account_Type.Current_Account)
+                        if (accountType == Account_Type.Current_Account)
                         {
                             Current_Account ca = new Current_Account();
                             ca.accountNo = dr.GetString(0);
-                            ca.name = dr.GetString(1);
-                            ca.address_line_1 = dr.GetString(2);
-                            ca.address_line_2 = dr.GetString(3);
-                            ca.address_line_3 = dr.GetString(4);
-                            ca.town = dr.GetString(5);
+
+                            ca.name = encryptionHelper.DecryptString(dr.GetString(1));
+                            ca.address_line_1 = encryptionHelper.DecryptString(dr.GetString(2));
+                            ca.address_line_2 = encryptionHelper.DecryptString(dr.GetString(3));
+                            ca.address_line_3 = encryptionHelper.DecryptString(dr.GetString(4));
+                            ca.town = encryptionHelper.DecryptString(dr.GetString(5));
+
                             ca.balance = dr.GetDouble(6);
                             ca.overdraftAmount = dr.GetDouble(8);
                             accounts.Add(ca);
@@ -101,27 +99,24 @@ namespace Banking_Application
                         {
                             Savings_Account sa = new Savings_Account();
                             sa.accountNo = dr.GetString(0);
-                            sa.name = dr.GetString(1);
-                            sa.address_line_1 = dr.GetString(2);
-                            sa.address_line_2 = dr.GetString(3);
-                            sa.address_line_3 = dr.GetString(4);
-                            sa.town = dr.GetString(5);
+
+                            sa.name = encryptionHelper.DecryptString(dr.GetString(1));
+                            sa.address_line_1 = encryptionHelper.DecryptString(dr.GetString(2));
+                            sa.address_line_2 = encryptionHelper.DecryptString(dr.GetString(3));
+                            sa.address_line_3 = encryptionHelper.DecryptString(dr.GetString(4));
+                            sa.town = encryptionHelper.DecryptString(dr.GetString(5));
+
                             sa.balance = dr.GetDouble(6);
                             sa.interestRate = dr.GetDouble(9);
                             accounts.Add(sa);
                         }
-
-
                     }
-
                 }
-
             }
         }
 
-        public String addBankAccount(Bank_Account ba) 
+        public String addBankAccount(Bank_Account ba)
         {
-
             if (ba.GetType() == typeof(Current_Account))
                 ba = (Current_Account)ba;
             else
@@ -133,68 +128,68 @@ namespace Banking_Application
             {
                 connection.Open();
                 var command = connection.CreateCommand();
-                command.CommandText =
-                @"
-                    INSERT INTO Bank_Accounts VALUES(" +
-                    "'" + ba.accountNo + "', " +
-                    "'" + ba.name + "', " +
-                    "'" + ba.address_line_1 + "', " +
-                    "'" + ba.address_line_2 + "', " +
-                    "'" + ba.address_line_3 + "', " +
-                    "'" + ba.town + "', " +
-                    ba.balance + ", " +
-                    (ba.GetType() == typeof(Current_Account) ? 1 : 2) + ", ";
+
+                string encryptedName = encryptionHelper.EncryptString(ba.name);
+                string encryptedAddressLine1 = encryptionHelper.EncryptString(ba.address_line_1);
+                string encryptedAddressLine2 = encryptionHelper.EncryptString(ba.address_line_2);
+                string encryptedAddressLine3 = encryptionHelper.EncryptString(ba.address_line_3);
+                string encryptedTown = encryptionHelper.EncryptString(ba.town);
+
+                command.CommandText = @"
+                    INSERT INTO Bank_Accounts 
+                    (accountNo, name, address_line_1, address_line_2, address_line_3, town, balance, accountType, overdraftAmount, interestRate)
+                    VALUES (@accNo, @name, @addr1, @addr2, @addr3, @town, @balance, @type, @od, @ir)";
+
+                command.Parameters.AddWithValue("@accNo", ba.accountNo);
+                command.Parameters.AddWithValue("@name", encryptedName ?? string.Empty);
+                command.Parameters.AddWithValue("@addr1", encryptedAddressLine1 ?? string.Empty);
+                command.Parameters.AddWithValue("@addr2", encryptedAddressLine2 ?? string.Empty);
+                command.Parameters.AddWithValue("@addr3", encryptedAddressLine3 ?? string.Empty);
+                command.Parameters.AddWithValue("@town", encryptedTown ?? string.Empty);
+                command.Parameters.AddWithValue("@balance", ba.balance);
+                command.Parameters.AddWithValue("@type", ba.GetType() == typeof(Current_Account) ? 1 : 2);
 
                 if (ba.GetType() == typeof(Current_Account))
                 {
                     Current_Account ca = (Current_Account)ba;
-                    command.CommandText += ca.overdraftAmount + ", NULL)";
+                    command.Parameters.AddWithValue("@od", ca.overdraftAmount);
+                    command.Parameters.AddWithValue("@ir", DBNull.Value);
                 }
-
                 else
                 {
                     Savings_Account sa = (Savings_Account)ba;
-                    command.CommandText += "NULL," + sa.interestRate + ")";
+                    command.Parameters.AddWithValue("@od", DBNull.Value);
+                    command.Parameters.AddWithValue("@ir", sa.interestRate);
                 }
 
                 command.ExecuteNonQuery();
-
             }
 
             return ba.accountNo;
-
         }
 
-        public Bank_Account findBankAccountByAccNo(String accNo) 
-        { 
-        
-            foreach(Bank_Account ba in accounts)
+        public Bank_Account findBankAccountByAccNo(String accNo)
+        {
+            foreach (Bank_Account ba in accounts)
             {
-
                 if (ba.accountNo.Equals(accNo))
                 {
                     return ba;
                 }
-
             }
-
-            return null; 
+            return null;
         }
 
-        public bool closeBankAccount(String accNo) 
+        public bool closeBankAccount(String accNo)
         {
-
             Bank_Account toRemove = null;
-            
             foreach (Bank_Account ba in accounts)
             {
-
                 if (ba.accountNo.Equals(accNo))
                 {
                     toRemove = ba;
                     break;
                 }
-
             }
 
             if (toRemove == null)
@@ -207,88 +202,77 @@ namespace Banking_Application
                 {
                     connection.Open();
                     var command = connection.CreateCommand();
-                    command.CommandText = "DELETE FROM Bank_Accounts WHERE accountNo = '" + toRemove.accountNo + "'";
+                    command.CommandText = "DELETE FROM Bank_Accounts WHERE accountNo = @accNo";
+                    command.Parameters.AddWithValue("@accNo", toRemove.accountNo);
                     command.ExecuteNonQuery();
-
                 }
 
                 return true;
             }
-
         }
 
         public bool lodge(String accNo, double amountToLodge)
         {
-
             Bank_Account toLodgeTo = null;
-
             foreach (Bank_Account ba in accounts)
             {
-
                 if (ba.accountNo.Equals(accNo))
                 {
                     ba.lodge(amountToLodge);
                     toLodgeTo = ba;
                     break;
                 }
-
             }
 
             if (toLodgeTo == null)
                 return false;
             else
             {
-
                 using (var connection = getDatabaseConnection())
                 {
                     connection.Open();
                     var command = connection.CreateCommand();
-                    command.CommandText = "UPDATE Bank_Accounts SET balance = " + toLodgeTo.balance + " WHERE accountNo = '" + toLodgeTo.accountNo + "'";
+                    command.CommandText = "UPDATE Bank_Accounts SET balance = @balance WHERE accountNo = @accNo";
+                    command.Parameters.AddWithValue("@balance", toLodgeTo.balance);
+                    command.Parameters.AddWithValue("@accNo", toLodgeTo.accountNo);
                     command.ExecuteNonQuery();
-
                 }
 
                 return true;
             }
-
         }
 
         public bool withdraw(String accNo, double amountToWithdraw)
         {
-
             Bank_Account toWithdrawFrom = null;
             bool result = false;
-
             foreach (Bank_Account ba in accounts)
             {
-
                 if (ba.accountNo.Equals(accNo))
                 {
                     result = ba.withdraw(amountToWithdraw);
                     toWithdrawFrom = ba;
                     break;
                 }
-
             }
 
             if (toWithdrawFrom == null || result == false)
                 return false;
             else
             {
-
                 using (var connection = getDatabaseConnection())
                 {
                     connection.Open();
                     var command = connection.CreateCommand();
-                    command.CommandText = "UPDATE Bank_Accounts SET balance = " + toWithdrawFrom.balance + " WHERE accountNo = '" + toWithdrawFrom.accountNo + "'";
+                    command.CommandText = "UPDATE Bank_Accounts SET balance = @balance WHERE accountNo = @accNo";
+                    command.Parameters.AddWithValue("@balance", toWithdrawFrom.balance);
+                    command.Parameters.AddWithValue("@accNo", toWithdrawFrom.accountNo);
                     command.ExecuteNonQuery();
-
                 }
 
                 return true;
             }
-
         }
-
     }
 }
+
---
+++ SSD Assignment - Banking Application/EncryptionHelper.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Banking_Application
+{
+    public class EncryptionHelper
+    {
+        private static EncryptionHelper instance = new EncryptionHelper();
+        private Aes aes;
+        private const string CRYPTO_KEY_NAME = "BankingAppAESKey";
+
+        private EncryptionHelper()
+        {
+            InitializeEncryption();
+        }
+
+        public static EncryptionHelper GetInstance()
+        {
+            return instance;
+        }
+
+        private void InitializeEncryption()
+        {
+            CngProvider keyStorageProvider = CngProvider.MicrosoftSoftwareKeyStorageProvider;
+
+            if (!CngKey.Exists(CRYPTO_KEY_NAME, keyStorageProvider))
+            {
+                CngKeyCreationParameters keyCreationParams = new CngKeyCreationParameters()
+                {
+                    Provider = keyStorageProvider,
+                    KeyUsage = CngKeyUsages.AllUsages
+                };
+
+                CngKey.Create(new CngAlgorithm("AES"), CRYPTO_KEY_NAME, keyCreationParams);
+            }
+
+            aes = new AesCng(CRYPTO_KEY_NAME, keyStorageProvider);
+            aes.KeySize = 256; 
+            aes.Mode = CipherMode.CBC; 
+            aes.Padding = PaddingMode.PKCS7; 
+        }
+
+     
+        public string EncryptString(string plaintext)
+        {
+            if (string.IsNullOrEmpty(plaintext))
+                return plaintext; 
+
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+            byte[] iv = new byte[16]; 
+            rng.GetBytes(iv);
+
+            byte[] plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+
+            aes.IV = iv;
+
+            byte[] ciphertextBytes = Encrypt(plaintextBytes, aes);
+
+            byte[] ivAndCiphertext = new byte[iv.Length + ciphertextBytes.Length];
+            Buffer.BlockCopy(iv, 0, ivAndCiphertext, 0, iv.Length);
+            Buffer.BlockCopy(ciphertextBytes, 0, ivAndCiphertext, iv.Length, ciphertextBytes.Length);
+
+            return Convert.ToBase64String(ivAndCiphertext);
+        }
+
+        public string DecryptString(string encryptedBase64)
+        {
+            if (string.IsNullOrEmpty(encryptedBase64))
+                return encryptedBase64; 
+
+            byte[] ivAndCiphertext = Convert.FromBase64String(encryptedBase64);
+
+            byte[] iv = new byte[16];
+            Buffer.BlockCopy(ivAndCiphertext, 0, iv, 0, iv.Length);
+
+            byte[] ciphertextBytes = new byte[ivAndCiphertext.Length - iv.Length];
+            Buffer.BlockCopy(ivAndCiphertext, iv.Length, ciphertextBytes, 0, ciphertextBytes.Length);
+
+            aes.IV = iv;
+
+            byte[] plaintextBytes = Decrypt(ciphertextBytes, aes);
+
+            return Encoding.UTF8.GetString(plaintextBytes);
+        }
+
+        private byte[] Encrypt(byte[] plaintextData, Aes aes)
+        {
+            byte[] ciphertextData;
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    csEncrypt.Write(plaintextData, 0, plaintextData.Length);
+                    csEncrypt.FlushFinalBlock();
+                }
+                ciphertextData = msEncrypt.ToArray();
+            }
+
+            return ciphertextData;
+        }
+
+        private byte[] Decrypt(byte[] ciphertextData, Aes aes)
+        {
+            byte[] plaintextData;
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+
+            using (MemoryStream msDecrypt = new MemoryStream())
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                {
+                    csDecrypt.Write(ciphertextData, 0, ciphertextData.Length);
+                    csDecrypt.FlushFinalBlock();
+                }
+                plaintextData = msDecrypt.ToArray();
+            }
+
+            return plaintextData;
+        }
+    }
+}
